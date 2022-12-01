7b75a7137f6463e730acbd1d52b29db3889f8e2e
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-12-01 17:07:15 +0000

    Day One Completed

---
+++ 1/calorie-counting.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <numeric>
+int main()
+{
+  std::string filename("input.txt");
+  std::vector<int> inventory;
+  std::ifstream input_file(filename);
+  if (!input_file.is_open())
+  {
+    std::cerr << "Could not open the file - '"
+              << filename << "'\n";
+    return EXIT_FAILURE;
+  }
+  std::string line;
+  while (getline(input_file, line))
+  {
+    int elfTotal{};
+    while (!line.empty())
+    {
+      int value = stoi(line);
+      elfTotal += value;
+      if (!getline(input_file, line))
+        break;
+    }
+    inventory.push_back(elfTotal);
+  }
+  std::sort(inventory.begin(), inventory.end(), std::greater<int>());
+  int value{};
+  std::cout << "Max: " << inventory[0] << "\n";
+  std::cout << std::accumulate(inventory.begin(), inventory.begin() + 3, 0);
+}
